dfadb605403ef796453616430fec8782fffb7867
MilanPatel2003 <131247110+MilanPatel2003@users.noreply.github.com>
2024-10-09 17:43:48 +0530

    Fix: mobile animation screen reload #2.1

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { lazy, Suspense } from 'react';
+import  { lazy, Suspense } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import StarsCanvas from "./components/ui/StarBackground";
 import Navbar from "./sections/navbar/Navbar";
